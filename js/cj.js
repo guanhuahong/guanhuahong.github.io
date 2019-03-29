eb63a0fca6e0b4d64c55c1b7b545e11a7a808f34
jackethong <372406020@qq.com>
2019-03-29 14:53:16 +0800

    添加支持同时多个抽奖

--- js/cj.js
+++ js/cj.js
@@ -2,10 +2,14 @@
     var PrizeDraw = exports.PrizeDraw = function() {
         this._number = 0;
         this.seed = 0;
+        this.same = 1;
     }
 
     PrizeDraw.prototype.setNumber = function(value) { this._number = value; }
     PrizeDraw.prototype.getNumber = function() { return this._number; }
+
+    PrizeDraw.prototype.setSame = function(value) { this._same = value; }
+    PrizeDraw.prototype.getSame = function() { return this._same; }
 
     PrizeDraw.prototype.isRun = function() { return this._run; }
 
@@ -13,10 +17,7 @@
     PrizeDraw.prototype.stop = function() {
         this._run = false;
 
-        var seed = Math.floor(this._number * Math.random());
-        if (seed >= this._number) {
-            seed = this._number - 1;
-        }
+        var seed = newSeed(this._number, this._same);
 
         if (this.onDraw) { 
             this.onDraw(seed)
@@ -31,13 +32,37 @@
         }
     }
 
+    function newSeed(n, s) {
+        s = s > 1 ? s : 1
+        var seed;
+        if (s === 1) {
+            seed = Math.floor(n * Math.random());
+            if (seed >= n) {
+                seed = n - 1;
+            }
+        } else {
+            var seed = [];
+            while(s > 0) {
+                r = Math.floor(n * Math.random());
+
+                if (seed.indexOf(r) <= -1) {
+                    if (r >= n) {
+                        r = n - 1
+                    }
+                    seed.push(r)
+                    s = s - 1;
+                }
+            }
+        }
+        
+
+        return seed
+    }
+
     PrizeDraw.prototype.update = function() {
         if (!this._run) return;
         if (this._number <= 0) return;
-        var seed = Math.floor(this._number * Math.random());
-        if (seed >= this._number) {
-            seed = this._number - 1;
-        }
+        var seed = newSeed(this._number, this._same);
 
         if (this.onDraw) { 
             this.onDraw(seed)
